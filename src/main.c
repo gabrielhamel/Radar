42c5a9ff4e69a0948e09deca315fc35dbe3bf101
Gabouchet <gabriel.hamel.pro@gmail.com>
2022-12-08 02:29:35 +0100

    fix(build): Removed vcpkg dependencies

--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
 #include <SFML/Graphics.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    sfVideoMode mode = sfVideoMode_getDesktopMode();
+sfRenderWindow *window_create(void)
+{
+    sfVideoMode mode = {800, 600, 32};
+    sfRenderWindow *window = NULL;
 
-    printf("%d %d %d", mode.width, mode.height, mode.bitsPerPixel);
-    return 0;
+    if (sfVideoMode_isValid(mode) == sfFalse) {
+        perror("Video mode not supported");
+        return NULL;
+    }
+    window = sfRenderWindow_create(mode, "Radar", sfDefaultStyle, NULL);
+    if (window == NULL) {
+        perror("Cannot open the required window");
+        return NULL;
+    }
+    sfRenderWindow_setFramerateLimit(window, 60);
+    sfRenderWindow_setVerticalSyncEnabled(window, sfTrue);
+    sfRenderWindow_setKeyRepeatEnabled(window, sfFalse);
+    return window;
+}
+
+int main()
+{
+    sfRenderWindow *window = window_create();
+
+    if (window == NULL) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
